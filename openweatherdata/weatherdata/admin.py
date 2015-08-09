64c2b771d06e7acf784e7d8e80eea1f4538094e0
Sammy Fung <sammy@sammy.hk>
2015-08-09 13:33:09 +0800

    updated django TropicalCyclone admin ui

--- openweatherdata/weatherdata/admin.py
+++ openweatherdata/weatherdata/admin.py
@@ -5,6 +5,8 @@ from weatherdata.models import TropicalCyclone
 
 class TropicalCycloneAdmin(admin.ModelAdmin):
   list_display = ('report_time', 'agency', 'code', 'name', 'position_type', 'position_time', 'latitude', 'longitude',
-                  'cyclone_type', 'wind_speed', 'wind_unit', 'pressure')
+                  'cyclone_type', 'wind_speed', 'gust_speed', 'wind_unit', 'pressure')
+  #list_filter = ('agency', 'position_type', 'cyclone_type', 'code')
+  search_fields = ['name', 'agency', 'code', 'cyclone_type', 'position_type']
 
 admin.site.register(TropicalCyclone, TropicalCycloneAdmin)
